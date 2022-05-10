d428bf000194bcb886056788ade8cbf3acf5aef7
conde <developer.luis.conde@gmail.com>
2022-05-09 21:47:15 -0500

    master | add base code

---
+++ openaq/openaq/src/test/java/com/oracle/mx/openaq/OpenaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.mx.openaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
